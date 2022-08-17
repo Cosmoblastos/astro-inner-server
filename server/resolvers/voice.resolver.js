eaa0f5cc000362242ee6c8f81c522f0b53fcc2b5
Alejandro Gómez García <alejandro@bluepixel.mx>
2022-08-16 20:05:49 -0500

    Feature: add variables recognition and insertion in voice instruction

--- server/resolvers/voice.resolver.js
+++ server/resolvers/voice.resolver.js
@@ -1,19 +1,6 @@
 const voice = require('../../lib/voice');
 const {pubsub} = require("../../lib/core");
 
-const instruction_set = [
-    {
-        say: 'Hello, I\'m Astro, your personal medical assistant',
-    },
-    {
-        say: 'What is your name?',
-        waitForResponse: true,
-    },
-    {
-        say: `Nice to meet you, Alejandro, I'm going to guide you through a set questions to know a little more about you`,
-    }
-];
-
 module.exports = {
     Subscription: {
         voiceEvents: {
